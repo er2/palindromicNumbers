d9b319c838a3c1b4fedc6b17ecad3f79859e4f3b
Eric Riese <eric.riese@gmail.com>
2015-06-13 15:46:32 -0400

    use longs to handle one challenge input recursive helper function is the only necessary one fix typo confession: I resorted to recursion after struggling with ownership system much of the rust syntax reminds me of scala, where recursion is encouraged
    This isn't a good place to use recursion, since a Lychrel number would
    cause a stack overflow. But I'm not using arbitrary precision arithmetic
    anyway, so I'm hitting integer overflow first anyway.

--- src/main.rs
+++ src/main.rs
@@ -2,40 +2,36 @@ use std::env;
 
 fn main() {
     for input in env::args().skip(1) {
-        match input.parse::<i32>() {
+        match input.parse::<i64>() {
             Ok(orig) => {
-                let (result, count) = get_pal(orig);
+                let (result, count) = get_pal(orig, 0);
                 println!("{} gets palindromic after {} steps: {}",
                     orig, count, result);
             }
             Err(e) => {
-                println!("error parsing intput: {} {:?}", input, e);
+                println!("error parsing input: {} {:?}", input, e);
             }
         }
     }
 }
 
-fn get_pal(i: i32) -> (i32, i32) {
-    get_pal_helper(i, 0)
-}
-
-fn get_pal_helper(i: i32, count: i32) -> (i32, i32) {
+fn get_pal(i: i64, count: i64) -> (i64, i64) {
     if i.is_palindrome() {
         (i, count)
     } else {
-        get_pal_helper(i + i.flip(), count+1)
+        get_pal(i + i.flip(), count+1)
     }
 }
 
 trait FlipInt {
-    fn flip(&self) -> i32;
+    fn flip(&self) -> i64;
     fn is_palindrome(&self) -> bool;
 }
 
-impl FlipInt for i32 {
-    fn flip(&self) -> i32 {
+impl FlipInt for i64 {
+    fn flip(&self) -> i64 {
         reverse(self.to_string())
-            .parse::<i32>().unwrap()
+            .parse::<i64>().unwrap()
     }
 
     fn is_palindrome(&self) -> bool {
